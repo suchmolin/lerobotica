c8676971fb1349964bd154900452dca0c5e189f7
suchmolin <suchmolin11@gmail.com>
2024-10-30 17:51:58 -0400

    [fix] lazy loading fotos soluciones, desactivada

--- src/components/BoxSolutionSelected/page.jsx
+++ src/components/BoxSolutionSelected/page.jsx
@@ -21,6 +21,7 @@ export default function BoxSolutionSelected({ data }) {
             objectFit="contain"
             layout="fill"
             alt={data.titulo}
+            priority
           />
         </div>
       </div>
