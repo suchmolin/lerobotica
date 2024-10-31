20fd30d4d335c9c4cc08186f67095288c9305359
suchmolin <suchmolin11@gmail.com>
2024-10-31 17:13:38 -0400

    helicoptero move, count up

--- src/components/CounterHeader/page.jsx
+++ src/components/CounterHeader/page.jsx
@@ -1,15 +1,26 @@
+"use client"
+import CountUp from "react-countup"
+
 export default function CounterHeader() {
   return (
     <div className="w-11/12 sm:w-fit bg-amarilloLR text-azulLR sm:px-16 flex flex-col sm:flex-row justify-between items-center py-4 rounded-t-2xl gap-6 xl:gap-10">
       <div className=" flex gap-6 xl:gap-10">
         <div className="flex flex-col text-center">
-          <p className="text-5xl font-[baloo-bold]">42</p>
+          <CountUp
+            className="text-5xl font-[baloo-bold]"
+            end={42}
+            duration={4}
+          />
           <p className="text-xl leading-none font-bold">
             Años de <br /> experiencia
           </p>
         </div>
         <div className="flex flex-col text-center">
-          <p className="text-5xl font-[baloo-bold]">66</p>
+          <CountUp
+            className="text-5xl font-[baloo-bold]"
+            end={66}
+            duration={4}
+          />
           <p className="text-xl leading-none font-bold">
             Paises <br /> alcanzados
           </p>
@@ -17,13 +28,21 @@ export default function CounterHeader() {
       </div>
       <div className=" flex gap-6 xl:gap-10">
         <div className="flex flex-col text-center">
-          <p className="text-5xl font-[baloo-bold]">65</p>
+          <CountUp
+            className="text-5xl font-[baloo-bold]"
+            end={65}
+            duration={4}
+          />
           <p className="text-xl leading-none font-bold">
             Millones de <br /> usuarios
           </p>
         </div>
         <div className="flex flex-col text-center">
-          <p className="text-5xl font-[baloo-bold]">22</p>
+          <CountUp
+            className="text-5xl font-[baloo-bold]"
+            end={22}
+            duration={4}
+          />
           <p className="text-xl leading-none font-bold">
             Soluciones <br /> creadas
           </p>
--- src/components/HelicopterMove/page.jsx
+++ src/components/HelicopterMove/page.jsx
@@ -1,10 +1,23 @@
 "use client"
 import Image from "next/image"
+import { useEffect } from "react"
 
 export default function HelicopterMove() {
+  useEffect(() => {
+    let body = document.querySelector("body")
+    let helicopter = document.querySelector(".helicopter")
+
+    body.addEventListener("mousemove", (e) => {
+      let x = 20 - e.clientX * 0.07
+      let y = 20 - e.clientY * 0.07
+
+      helicopter.style.transform = `translate(${x}px, ${y}px)`
+    })
+  }, [])
+
   return (
-    <div className=" absolute top-0 left-0 lg:-left-20">
-      <div className="w-[350px] h-[250px] aspect-square relative overflow-hidden flex justify-center items-center">
+    <div className=" absolute top-5 left-20 lg:left-0 lg:-left-5">
+      <div className="w-[350px] h-[250px] aspect-square relative  flex justify-center items-center">
         <div className="absolute top-0 left-0 w-full h-full object-cover">
           <div className="helicopter w-[180px] sm:w-[300px] aspect-square relative">
             <Image
