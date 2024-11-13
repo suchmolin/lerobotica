333a1a09e06868b795de67099cdb4967d2654eea
suchmolin <suchmolin11@gmail.com>
2024-11-13 15:12:46 -0400

    [test] tamaño del header3

--- src/components/CounterHeader/page.jsx
+++ src/components/CounterHeader/page.jsx
@@ -3,7 +3,7 @@ import CountUp from "react-countup"
 
 export default function CounterHeader() {
   return (
-    <div className="w-11/12 sm:w-fit bg-amarilloLR text-azulLR sm:px-16 flex flex-col sm:flex-row justify-between items-center py-4 rounded-t-2xl gap-6 xl:gap-10">
+    <div className="w-11/12 sm:w-fit bg-amarilloLR text-azulLR sm:px-16 flex flex-col sm:flex-row justify-between items-center py-4 rounded-t-2xl gap-6 xl:gap-10 dropShadow5 mb-10">
       <div className=" flex gap-6 xl:gap-10">
         <div className="flex flex-col text-center">
           <CountUp
