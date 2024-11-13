8ca96e0858c62e75b441e684bdd27ce04fd8fcd4
suchmolin <suchmolin11@gmail.com>
2024-11-13 14:58:46 -0400

    [test] tamaño del header

--- src/components/HeaderHome/page.jsx
+++ src/components/HeaderHome/page.jsx
@@ -6,7 +6,7 @@ import CounterHeader from "../CounterHeader/page"
 
 export default function HeaderHome(props) {
   return (
-    <div className="firstSec motionWrapper relative w-full lg:h-[calc(100vh-125px)] flex flex-col lg:flex-row justify-end overflow-hidden mb-10">
+    <div className="firstSec motionWrapper relative w-full lg:h-[740px] flex flex-col lg:flex-row justify-end overflow-hidden mb-10">
       <div className="relative w-full lg:w-5/12 flex flex-col justify-center sm:pl-10 lg:pl-20 mt-5 mb-5 sm:mb-20 z-10">
         <h1 className="text-azulLR text-4xl xs:text-5xl sm:text-6xl lg:text-7xl font-[baloo-bold] px-3 xs:px-8 sm:px-0">
           Robótica y
