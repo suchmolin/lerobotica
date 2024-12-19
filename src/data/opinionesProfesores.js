6488f23398be6a341e13d13995cd5d10d76202dc
suchmolin <suchmolin11@gmail.com>
2024-12-19 14:07:31 -0400

    videos de entrevista a los profesores

--- src/data/opinionesProfesores.js
+++ src/data/opinionesProfesores.js
@@ -1,22 +1,22 @@
 export const data = [
   {
     id: "video1",
-    src: "https://firebasestorage.googleapis.com/v0/b/jsuchmolin-394bd.appspot.com/o/Video%2060%20Landing%20page_1.mp4?alt=media&token=89ef3772-33bf-41ca-bfe4-9286d7dcab33",
+    src: "https://firebasestorage.googleapis.com/v0/b/jsuchmolin-394bd.appspot.com/o/ENTREVISTAANA%202.mp4?alt=media&token=71e680ec-cd92-4128-a66d-725c94eaea57",
     poster: "/img/poster1.png",
   },
   {
     id: "video2",
-    src: "https://firebasestorage.googleapis.com/v0/b/jsuchmolin-394bd.appspot.com/o/Video%2060%20Landing%20page_1.mp4?alt=media&token=89ef3772-33bf-41ca-bfe4-9286d7dcab33",
+    src: "https://firebasestorage.googleapis.com/v0/b/jsuchmolin-394bd.appspot.com/o/ENTREVISTAJUAN%202.mp4?alt=media&token=1f06eb88-c7dd-419d-b3b3-15440b5ea69e",
     poster: "/img/poster2.png",
   },
   {
     id: "video3",
-    src: "https://firebasestorage.googleapis.com/v0/b/jsuchmolin-394bd.appspot.com/o/Video%2060%20Landing%20page_1.mp4?alt=media&token=89ef3772-33bf-41ca-bfe4-9286d7dcab33",
+    src: "https://firebasestorage.googleapis.com/v0/b/jsuchmolin-394bd.appspot.com/o/ENTREVISTAYEIMA%202.mp4?alt=media&token=96cf48b7-2125-423d-9500-bb897614d403",
     poster: "/img/poster3.png",
   },
   {
     id: "video4",
-    src: "https://firebasestorage.googleapis.com/v0/b/jsuchmolin-394bd.appspot.com/o/Video%2060%20Landing%20page_1.mp4?alt=media&token=89ef3772-33bf-41ca-bfe4-9286d7dcab33",
+    src: "https://firebasestorage.googleapis.com/v0/b/jsuchmolin-394bd.appspot.com/o/ENTREVISTAANDRE%202.mp4?alt=media&token=8e33559a-d41f-46ee-82b1-ed08790ae535",
     poster: "/img/poster4.png",
   },
 ]
