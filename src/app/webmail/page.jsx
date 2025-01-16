62d62d24b0ce08c5836374fe6e49fe9b2e850e70
suchmolin <suchmolin11@gmail.com>
2025-01-16 10:36:11 -0400

    ruta webmail seted

---
+++ src/app/webmail/page.jsx
@@ -0,0 +1,12 @@
+"use client"
+import React, { useEffect } from "react"
+
+export default function WebmailPage() {
+  useEffect(() => {
+    window.location.replace(
+      "https://p3plzcpnl508777.prod.phx3.secureserver.net:2096/"
+    )
+  }, [])
+
+  return null
+}
