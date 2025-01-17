442dad59578b4853b2e336bf4c5a421891888da7
suchmolin <suchmolin11@gmail.com>
2025-01-17 16:40:10 -0400

    cambio de copy home

--- src/data/diferenciadores.js
+++ src/data/diferenciadores.js
@@ -1,21 +1,21 @@
 export const data = [
   {
     id: "1",
-    title: "¡Aprende jugando!",
+    title: "¡Aprenden jugando!",
     description:
       "Tu hijo aprende divirtiéndose a través de clases dinámicas y juegos. Aprender mediante el juego fomenta el compromiso de los niños con las clases y en LeRobotica lo sabemos.",
     icon: "/img/difBombillo.png",
   },
   {
     id: "2",
-    title: "Expresa ideas, explora soluciones",
+    title: "Expresan ideas, exploran soluciones",
     description:
       "En LeRobótica, la imaginación de tu hijo no tendrá límites. A través de actividades creativas y construcciones innovadoras, aprende a transformar sus ideas en realidad.",
     icon: "/img/difLego.png",
   },
   {
     id: "3",
-    title: "Desarrolla sus habilidades blandas",
+    title: "Desarrollan sus habilidades blandas",
     description:
       "Tu hijo desarrolla habilidades clave para la vida como el trabajo en equipo, el pensamiento lógico y la resolución de problemas.",
     icon: "/img/difCohete.png",
